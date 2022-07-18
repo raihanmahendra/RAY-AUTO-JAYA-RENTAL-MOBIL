ccea94a87762156fbe8cfe2b2c839c114a0a4627
raihanmahendra <raihan20ti@mahasiswa.pcr.ac.id>
2022-07-18 11:48:26 +0700

    Commit Pertama

---
+++ application/views/customer/dashboard.php
@@ -0,0 +1,170 @@
+<!-- Page Content -->
+<!-- Banner Starts Here -->
+<div class="main-banner header-text">
+  <div class="container-fluid">
+    <div class="owl-banner owl-carousel">
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/product-1-720x480.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+              <span>
+                <i class="fa fa-user"></i> 5 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-briefcase"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-sign-out"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-cog"></i> A
+              </span>
+            </div>
+            <a href="fleet.html">
+              <h4>MOBIL RENTAL.</h4>
+            </a>
+            <ul class="post-info">
+
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/product-2-720x480.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+              <span>
+                <i class="fa fa-user"></i> 5 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-briefcase"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-sign-out"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-cog"></i> A
+              </span>
+            </div>
+            <a href="fleet.html">
+              <h4>MOBIL RENTAL.</h4>
+            </a>
+            <ul class="post-info">
+
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/product-3-720x480.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+              <span>
+                <i class="fa fa-user"></i> 5 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-briefcase"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-sign-out"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-cog"></i> A
+              </span>
+            </div>
+            <a href="fleet.html">
+              <h4>MOBIL RENTAL.</h4>
+            </a>
+            <ul class="post-info">
+
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/product-4-720x480.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+              <span>
+                <i class="fa fa-user"></i> 5 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-briefcase"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-sign-out"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-cog"></i> A
+              </span>
+            </div>
+            <a href="fleet.html">
+              <h4>MOBIL RENTAL.</h4>
+            </a>
+            <ul class="post-info">
+
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/product-5-720x480.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+              <span>
+                <i class="fa fa-user"></i> 5 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-briefcase"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-sign-out"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-cog"></i> A
+              </span>
+            </div>
+            <a href="fleet.html">
+              <h4>MOBIL RENTAL.</h4>
+            </a>
+            <ul class="post-info">
+
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/product-6-720x480.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+              <span>
+                <i class="fa fa-user"></i> 5 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-briefcase"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-sign-out"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-cog"></i> A
+              </span>
+            </div>
+            <a href="fleet.html">
+              <h4>MOBIL RENTAL.</h4>
+            </a>
+            <ul class="post-info">
+
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Banner Ends Here -->
+
+<section class="blog-posts grid-system">
+  <div class="container">
+    <div class="all-blog-posts">
+    </div>
+  </div>
+</section>
+
+
+
+<section class="blog-posts grid-system">
+  <div class="container">
+    <div class="all-blog-posts">
+
+    </div>
+  </div>
+</section>
+
+<div class="blog-posts">
+  <div class="container">
+    <div class="sidebar-item comments">
+
+      <br>
+      <br>
+
+      <div class="row justify-content-md-center">
+        <div class="col-md-3">
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div style="height: 180px;"></div>
